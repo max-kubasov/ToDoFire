85bf2eac3cf05f030c39f87aac0c9b0c39b8286f
max-kubasov <kubasov87@gmail.com>
2023-01-02 08:41:45 +0200

    Add checkbox Inherit Module From Target

--- ToDoFire/LoginViewController.swift
+++ ToDoFire/LoginViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var warn: UILabel!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func registerTapped(_ sender: UIButton) {
+    }
 }
 
